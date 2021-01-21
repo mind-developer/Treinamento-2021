f660f5d713f53447c6efe51480c930c7be50d892
Pabolo Pires <pabolo18@gmail.com>
2021-01-21 18:17:49 -0300

    [Add] Route to Home

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import Login from "../screens/Login/Login";
 import Cadastro from "../screens/cadastro";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from '../screens/home/Home';
 export default function Routes() {
   return (
     <BrowserRouter basename="/">
@@ -10,6 +11,7 @@ export default function Routes() {
           path={`${process.env.PUBLIC_URL}/cadastro`}
           component={Cadastro}
         />
+        <Route exact path={`${process.env.PUBLIC_URL}/home`} component={Home} />
       </Switch>
     </BrowserRouter>
   );
